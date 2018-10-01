03c4afec5f6148b988b8449afd53f8e3dea70046
Nelson Huezoe <nelson.huezo88@gmail.com>
2018-10-01 18:00:17 -0400

    Adds a comment in the js file
    - Added a comment to explain the second argument in the parseInt
    function

--- javascript/stringToNumber.js
+++ javascript/stringToNumber.js
@@ -1,6 +1,8 @@
 let word = '1234'
 
 const stringToNumber = function(str){
+// The second argument in parseINt is called a radix. For our case it
+// should always be 10
   let number = parseInt(str, 10)
 console.log(number)
   return number;
